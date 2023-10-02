dd105261ffa78cd95f43c8ebc80272eb9d355454
abdulkalam1233 <abdulkalamshaik1233@gmail.com>
2023-10-02 12:30:13 +0530

    DEP: alpinejs integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import tailwindIntegration from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+import alpine from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwindIntegration()],
+  integrations: [tailwindIntegration(), alpine()],
 });
